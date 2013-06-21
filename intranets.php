710240e01dbd7de39f40e654cdc90c2003b7c691
Kevin Miller <kemiller@csumb.edu>
2013-06-21 15:27:50 -0600

    Update intranets.php

--- intranets.php
+++ intranets.php
@@ -3,13 +3,16 @@ $domains = file('domains.txt');
 $intranets = fopen('intranets.txt', 'w');
 $prefixes = array('my', 'portal', 'intranet');
 foreach($domains as $domain) {
-	$domain = '.'. trim($domain);
-	foreach($prefixes as $prefix) {
-		$result = dns_get_record($prefix . $domain);
-		print $prefix . $domain . "\n";
-		if($result && $result[0]['host']) {
-			fwrite($intranets, $result[0]['host'] ."\n");
-		}
-	}
+        $domain = '.'. trim($domain);
+        foreach($prefixes as $prefix) {
+                $result = dns_get_record($prefix . $domain);
+                if($result && $result[0]['host']) {
+                        $result = file_get_contents('http://'. $result[0]['host']);
+                        if($result) {
+                                fwrite($intranets, $result[0]['host'] ."\n");
+                                print $prefix . $domain ."\n";
+                        }
+                }
+        }
 }
 fclose($intranets);
