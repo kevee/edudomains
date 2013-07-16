38833366d536d02c79594fdaa4ba68e1286431b8
Kevin Miller <kemiller@csumb.edu>
2013-07-16 08:43:22 -0700

    Fixed generate php code

--- generate.php
+++ generate.php
@@ -2,37 +2,18 @@
 
 $alpha = range('a', 'z');
 $file = fopen('domains.txt', 'w');
+$extras = array('ca', 'co', 'ma', 'new', 'st', 'tc');
 $final = array();
 foreach($alpha as $a) {
   foreach($alpha as $b) {
-    foreach($alpha as $c) {
-      $output = array();
-        exec('whois '. $a . $b. $c.'%.edu', $output);
-        $i = 31;
-        $count = 0;
-        while(isset($output[$i])) {
-          $d = strtolower($output[$i]);
-          if(strpos($d, '@')) {
-            $d = explode('@', $d);
-            $d = $d[1];
-          }
-          if(strpos($d, '.edu') !== false) {
-            if(strpos($d, "\t")) {
-              $d = reset(explode("\t", $d));
-            }
-            if(strpos($d, ' ')) {
-              $d = reset(explode(" ", $d));
-            }
-            $d = parse_url('http://'. $d);
-            if($d['host']) {
-              $final[trim($d['host'])] = trim($d['host']);
-            }
-            $count++;
-          }
-          $i++;
-        }
-      print $a. $b . $c ." " . count($final) . "\n";
-    }
+    getDomains($a . $b, $final);
+    print $a. $b ." " . count($final) . "\n";
+  }
+}
+foreach($extras as $extra) {
+  foreach($alpha as $a) {
+    getDomains($extra . $a, $final);
+    print $extra . $a . " " . count($final) . "\n";
   }
 }
 ksort($final);
@@ -42,3 +23,31 @@ foreach($final as $d) {
   }
 }
 fclose($file);
+
+function getDomains($prefix, &$final) {
+  $output = array();
+  exec('whois '. $prefix.'%.edu', $output);
+  $i = 31;
+  $count = 0;
+  while(isset($output[$i])) {
+    $d = strtolower($output[$i]);
+    if(strpos($d, '@')) {
+      $d = explode('@', $d);
+      $d = $d[1];
+    }
+    if(strpos($d, '.edu') !== false) {
+      if(strpos($d, "\t")) {
+        $d = reset(explode("\t", $d));
+      }
+      if(strpos($d, ' ')) {
+        $d = reset(explode(" ", $d));
+      }
+      $d = parse_url('http://'. $d);
+      if($d['host']) {
+        $final[trim($d['host'])] = trim($d['host']);
+      }
+      $count++;
+    }
+    $i++;
+  }
+}
