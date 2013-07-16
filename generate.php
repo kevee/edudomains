133359dc7f2e15cf21b5b7d63683d3b19211369d
Kevin Miller <kemiller@csumb.edu>
2013-07-16 07:15:01 -0700

    Added another layer to the lookup.

--- generate.php
+++ generate.php
@@ -5,32 +5,34 @@ $file = fopen('domains.txt', 'w');
 $final = array();
 foreach($alpha as $a) {
   foreach($alpha as $b) {
+    foreach($alpha as $c) {
       $output = array();
-      exec('whois '. $a . $b. '%.edu', $output);
-      $i = 31;
-      $count = 0;
-      while(isset($output[$i])) {
-        $d = strtolower($output[$i]);
-        if(strpos($d, '@')) {
-          $d = explode('@', $d);
-          $d = $d[1];
+        exec('whois '. $a . $b. $c.'%.edu', $output);
+        $i = 31;
+        $count = 0;
+        while(isset($output[$i])) {
+          $d = strtolower($output[$i]);
+          if(strpos($d, '@')) {
+            $d = explode('@', $d);
+            $d = $d[1];
+          }
+          if(strpos($d, '.edu') !== false) {
+            if(strpos($d, "\t")) {
+              $d = reset(explode("\t", $d));
+            }
+            if(strpos($d, ' ')) {
+              $d = reset(explode(" ", $d));
+            }
+            $d = parse_url('http://'. $d);
+            if($d['host']) {
+              $final[trim($d['host'])] = trim($d['host']);
+            }
+            $count++;
+          }
+          $i++;
         }
-        if(strpos($d, '.edu') !== false) {
-          if(strpos($d, "\t")) {
-            $d = reset(explode("\t", $d));
-          }
-          if(strpos($d, ' ')) {
-            $d = reset(explode(" ", $d));
-          }
-          $d = parse_url('http://'. $d);
-          if($d['host']) {
-            $final[trim($d['host'])] = trim($d['host']);
-          }
-          $count++;
-        }
-        $i++;
-      }
-    print $a. $b . " " . count($final) . "\n";
+      print $a. $b . $c ." " . count($final) . "\n";
+    }
   }
 }
 ksort($final);
